e112adf4616e3adefedda9017ebb15c597e3193e
hsayadi4 <hamza.sayadi@epitech.digital>
2024-10-08 16:10:59 +0200

    Update ex_01.js

--- ex_01.js
+++ ex_01.js
@@ -1,4 +1,9 @@
-const rectangle = document.getElementById('square'); function reduceOpacity() {
-rectangle.style.opacity *= 0.5; } function resetOpacity() {
-rectangle.style.opacity = 1; } rectangle.addEventListener('mouseover',
-reduceOpacity); rectangle.addEventListener('mouseleave', resetOpacity);
+const rectangle = document.getElementById("square");
+function reduceOpacity() {
+  rectangle.style.opacity *= 0.5;
+}
+function resetOpacity() {
+  rectangle.style.opacity = 1;
+}
+rectangle.addEventListener("mouseover", reduceOpacity);
+rectangle.addEventListener("mouseleave", resetOpacity);
