0d7c60a7f2556565349d3d134e64f00a440fd5ae
hsayadi4 <hamza.sayadi@epitech.digital>
2024-10-08 16:07:13 +0200

    Create ex_01.js

---
+++ ex_01.js
@@ -0,0 +1,4 @@
+const rectangle = document.getElementById('square'); function reduceOpacity() {
+rectangle.style.opacity *= 0.5; } function resetOpacity() {
+rectangle.style.opacity = 1; } rectangle.addEventListener('mouseover',
+reduceOpacity); rectangle.addEventListener('mouseleave', resetOpacity);
